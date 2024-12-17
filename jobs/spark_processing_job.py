5ffeae41d289babaa60e696c4fd8e6902d572dac
Younes Bousetta <100391775+bousettayounes@users.noreply.github.com>
2024-12-17 17:16:55 +0100

    sparkjob insertion to DB and streaming to kafkatopic

--- jobs/spark_processing_job.py
+++ jobs/spark_processing_job.py
@@ -3,8 +3,8 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from datetime import datetime
-import json,time
-
+import json
+import threading
 
 CURRENT_TIME = datetime.now().strftime("%Y%m%d_%H%M%S")
 KAFKA_BROKERS = "kafka-broker-1:19091,kafka-broker-2:19092,kafka-broker-3:19093"
@@ -15,34 +15,29 @@ ANNOMALIES_TOPIC = "ANNOMALIES_TOPIC"
 CHECKPOINT_LOCATION = f"/opt/bitnami/spark/checkpoint/{CURRENT_TIME}"
 STATE_LOCATION = "/opt/bitnami/spark/state"
 SUBJECT_NAME = "Financial_TRANSACTIONS_schema"
+DB_URL = "jdbc:postgresql://postgres_db_transactions:5432/Transactions_DB"
+DB_PROPERTIES = {'user': 'yns', 'password': 'password', 'driver': 'org.postgresql.Driver'}
+TABLE_NAME = "Transactions"
 
 def get_schema_from_registry():
-
     try:
         schema_registry_client = SchemaRegistryClient({'url': SCHEMA_REGISTRY_URL})
-        
         latest_schema = schema_registry_client.get_latest_version(SUBJECT_NAME)
-        
         schema_str = latest_schema.schema.schema_str
-        
         return parse_avro_schema_to_spark_schema(json.loads(schema_str))
-    
     except Exception as e:
         print(f"ERROR RETRIEVING SCHEMA : {e}")
         raise
 
 def parse_avro_schema_to_spark_schema(avro_schema):
-
     fields = []
     for field in avro_schema['fields']:
         field_name = field['name']
         field_type = field['type']
-        
         if isinstance(field_type, list):
             actual_type = field_type[0] if field_type[0] != 'null' else field_type[1]
         else:
             actual_type = field_type
-        
         spark_type = {
             'string': StringType(),
             'long': LongType(),
@@ -50,9 +45,7 @@ def parse_avro_schema_to_spark_schema(avro_schema):
             'int': IntegerType(),
             'boolean': BooleanType()
         }.get(actual_type, StringType())
-        
         fields.append(StructField(field_name, spark_type, nullable=True))
-    
     return StructType(fields)
 
 spark = (SparkSession.builder
@@ -63,7 +56,6 @@ spark = (SparkSession.builder
          .getOrCreate())
 
 spark.sparkContext.setLogLevel("WARN")
-
 transaction_schema = get_schema_from_registry()
 
 kafka_stream = (spark.readStream
@@ -78,26 +70,51 @@ transactions_dataframe = (kafka_stream
                           .select(from_json(col('value'), transaction_schema).alias("Data"))
                           .select('Data.*'))
 
-transactions_dataframe = transactions_dataframe.withColumn(
-    'transactionTimestamp', (col('transactionTime') / 1000).cast('timestamp'))
+agg_transactions_dataframe = transactions_dataframe.withColumn(
+    'transactionTimestamp', (col('transactiontime') / 1000).cast('timestamp'))
 
-aggregated_df = transactions_dataframe.groupBy("merchantId") \
-    .agg(
-        sum("amount").alias("totalAmount"),
-        count("*").alias("transactionCount")
-    )
+def write_to_db(batch_df, batch_id):
+    batch_df.write.jdbc(url=DB_URL, table=TABLE_NAME, mode="append", properties=DB_PROPERTIES)
 
-aggregated_df.withColumn("key", col("merchantId").cast("string")) \
-    .withColumn("value", to_json(struct(
-        col("merchantId"),
-        col("totalAmount"),
-        col("transactionCount")
-    ))).selectExpr("key", "value") \
-    .writeStream \
-    .format("kafka") \
-    .outputMode("update") \
-    .option("kafka.bootstrap.servers", KAFKA_BROKERS) \
-    .option("topic", AGGREGATES_TOPIC) \
-    .option("partitionBy","key")\
-    .option("checkpointLocation", f"{CHECKPOINT_LOCATION}/aggregates") \
-    .start().awaitTermination()
+def write_to_database():
+    transactions_dataframe.writeStream \
+        .foreachBatch(write_to_db) \
+        .outputMode("append") \
+        .option("checkpointLocation", f"{CHECKPOINT_LOCATION}/db") \
+        .start()
+
+def write_aggregated_data():
+    aggregated_df = agg_transactions_dataframe.groupBy("merchantId") \
+        .agg(
+            sum("amount").alias("totalAmount"),
+            count("*").alias("transactionCount")
+        )
+
+    aggregated_df.withColumn("key", col("merchantId").cast("string")) \
+        .withColumn("value", to_json(struct(
+            col("merchantId"),
+            col("totalAmount"),
+            col("transactionCount")
+        )))\
+        .selectExpr("key", "value") \
+        .writeStream \
+        .format("kafka") \
+        .outputMode("update") \
+        .option("kafka.bootstrap.servers", KAFKA_BROKERS) \
+        .option("topic", AGGREGATES_TOPIC) \
+        .option("partitionBy", "key") \
+        .option("checkpointLocation", f"{CHECKPOINT_LOCATION}/aggregates") \
+        .start().awaitTermination()
+
+def run_parallel():
+    db_thread = threading.Thread(target=write_to_database)
+    kafka_thread = threading.Thread(target=write_aggregated_data)
+    
+    db_thread.start()
+    kafka_thread.start()
+    
+    db_thread.join()
+    kafka_thread.join()
+
+if __name__ == "__main__":
+    run_parallel()
